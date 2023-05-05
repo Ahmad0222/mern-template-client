9cbb3a20cf4d2b95a293c42203c0c2a03cded974
Ahmad0222 <03164322144ahmad@gmail.com>
2023-05-06 00:13:06 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import ProtectedRoute from './components/ProtectedRoute';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import FirstView from './views/FirstView';
+import FirstComponent from './components/FirstComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ProtectedRoute>
+            <FirstView />
+          </ProtectedRoute>} />
+
+          <Route path="/login" element={<FirstComponent />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
